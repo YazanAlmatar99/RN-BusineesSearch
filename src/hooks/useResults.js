d05c3615d8081c9ad3ca729da6183fe3a7cb908f
Yazan <yazanmatar90@gmail.com>
2020-06-09 18:50:40 -0400

    created useResults hook

---
+++ src/hooks/useResults.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import yelp from "../API/yelp";
+
+export default () => {
+  const [results, setResults] = useState([]);
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const searchApi = async (searchTerm) => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: "staten island",
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage("Something went wrong, Please try again.");
+    }
+  };
+  //the empty array that was passed as a second argument will make searchApi() get called only once
+  useEffect(() => {
+    searchApi("food");
+  }, []);
+  return [searchApi, results, errorMessage];
+};
